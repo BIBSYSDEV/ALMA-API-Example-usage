a2354a3d1a6107e1185fe89762fd9577537513e9
Per Christian Bjelke <pcb@unit.no>
2020-03-17 12:57:21 +0100

    ALMA-26--added working mockito-test for implementation.

--- src/test/java/no/unit/alma/acquisitions/AlmaVendorsClientTest.java
+++ src/test/java/no/unit/alma/acquisitions/AlmaVendorsClientTest.java
@@ -2,11 +2,14 @@ package no.unit.alma.acquisitions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,24 +31,36 @@ class AlmaVendorsClientTest {
     @Mock
     private WebTarget webTarget;
 
+    @Mock
+    private Invocation.Builder builder;
+
+    @Mock
+    private Invocation invocation;
+
+    @Mock
+    private Response response;
+
+
     @Test
     void testGetVendor() throws IOException, InterruptedException {
-        // setup
-//        Client client = JerseyClientBuilder.newClient();
-//        WebTarget webTarget = client.target("http://www.example.com/");
-
         // mock almaClient-methods
         when(this.mockAlmaApiClient.getWebTarget())
                 .thenReturn(webTarget);
         when(mockAlmaApiClient.getAlmaStage()).thenReturn(AlmaStage.SANDBOX2);
         when(mockAlmaApiClient.getContext()).thenReturn("");
         when(mockAlmaApiClient.getContextValue()).thenReturn("");
-        when(webTarget.path(any())).thenReturn(null);
+        when(webTarget.path(any())).thenReturn(webTarget);
+        when(webTarget.path(any())).thenReturn(webTarget);
+        when(webTarget.request()).thenReturn(builder);
+        when(builder.accept(anyString())).thenReturn(builder);
+        when(builder.buildGet()).thenReturn(invocation);
+        Vendor tempVendor = new Vendor();
+        tempVendor.setCode("test");
+        when(invocation.invoke((Class<Object>) any())).thenReturn(tempVendor);
         AlmaVendorsServiceImplementation almaVendorsServiceImplementation =
                 new AlmaVendorsServiceImplementation(mockAlmaApiClient);
-
         Vendor vendor = almaVendorsServiceImplementation.getVendor(TEST_ID);
-        assertEquals("test", vendor.getVendorLibraries());
+        assertEquals(tempVendor.getCode(), vendor.getCode());
     }
 
 }
