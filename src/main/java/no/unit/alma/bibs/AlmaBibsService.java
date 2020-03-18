14670ea6a378546380cb14227f143a9970391787
Eirik Nilsen <eirik.nilsen@unit.no>
2020-03-18 14:34:12 +0100

    Alma 34  lag test getbibs (#9)
    * copied file from alma-services
    
    * Removing code
    
    * Removed a lot of classes, renamed AlmaBibsClient to AlmaBibsService
    
    * Added Holdings and Items
    
    * Updated service factory with Holdings and Items
    
    * ALMA-34 Create testclass
    
    * ALMA-34 Testclass updated
    
    * ALMA-34 Merge fubar
    
    * ALMA-34 Added more mocking
    
    * ALMA-34 Mocking BIBS-services

--- src/main/java/no/unit/alma/bibs/AlmaBibsService.java
+++ src/main/java/no/unit/alma/bibs/AlmaBibsService.java
@@ -19,21 +19,29 @@ public interface AlmaBibsService extends AlmaContext {
 
     UserRequests getRequestsFromBib(String recordIdentifier, boolean deleted);
 
-    Representation createRemotePresentation(String barcode, String access, String digitalRepositoryId, String url, String libraryCode);
+    Representation createRemotePresentation(String barcode, String access, String digitalRepositoryId, String url,
+            String libraryCode);
 
-    Representation createRemotePresentation(String mmsId, String barcode, String access, String digitalRepositoryId, String url, String libraryCode);
+    Representation createRemoteRepresentation(String mmsId, String barcode, String access, String digitalRepositoryId,
+            String url, String libraryCode);
 
-    Representation createRemotePresentation(long mmsId, String access, String remoteRepositoryId, String libraryCode, String url, String year, String month, String day, String volume, String issue, String number);
+    Representation createRemoteRepresentation(long mmsId, String access, String remoteRepositoryId, String libraryCode,
+            String url, String year, String month, String day, String volume, String issue, String number);
 
-    Representation createRemotePresentation(String barcode, String access, String remoteRepositoryId, String libraryCode, String url, String year, String month, String day, String volume, String issue, String number);
+    Representation createRemoteRepresentation(String barcode, String access, String remoteRepositoryId,
+            String libraryCode, String url, String year, String month, String day, String volume, String issue,
+            String number);
 
-    Representation createRemotePresentation(String barcode, String label, String access, String remoteRepositoryId, String libraryCode, String url, String year, String month, String day, String volume, String issue, String number);
-
-    Representations getRemoteRepresentationsFromMmsId(String mmsId, int limit, int offset);
+    Representation createRemoteRepresentation(String barcode, String label, String access, String remoteRepositoryId,
+            String libraryCode, String url, String year, String month, String day, String volume, String issue,
+            String number);
 
     Representation getSingleRemoteRepresentationFromBarcode(String barcode, String representationId);
 
     Representation getSingleRemoteRepresentationFromMmsId(String mmsId, String representationId);
 
-    Bibs retrieveBibs(String mms_id, String ie_id, String holdings_id, String representation_id, String nz_mms_id, String view, String expand);
+    Representations getRemoteRepresentationsFromMmsId(String mmsId, int limit, int offset);
+
+    Bibs retrieveBibs(String mms_id, String ie_id, String holdings_id, String representation_id, String nz_mms_id,
+            String view, String expand);
 }
