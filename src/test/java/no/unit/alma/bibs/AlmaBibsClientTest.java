c6b7960b8871ffcdfdb1e44da513df36fa0927ad
Eirik Nilsen <eirik.nilsen@unit.no>
2020-03-18 06:45:56 +0100

    ALMA-34 Added more mocking

--- src/test/java/no/unit/alma/bibs/AlmaBibsClientTest.java
+++ src/test/java/no/unit/alma/bibs/AlmaBibsClientTest.java
@@ -2,10 +2,13 @@ package no.unit.alma.bibs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 
 import org.junit.jupiter.api.Test;
@@ -28,6 +31,12 @@ class AlmaBibsClientTest {
     @Mock
     private WebTarget webTarget;
 
+    @Mock
+    private Builder builder;
+
+    @Mock
+    private Invocation invocation;
+
     @Test
     void testGetBib() throws IOException, InterruptedException {
         // setup
@@ -35,16 +44,24 @@ class AlmaBibsClientTest {
 //        WebTarget webTarget = client.target("http://www.example.com/");
 
         // mock almaClient-methods
+
         when(this.mockAlmaApiClient.getWebTarget())
                 .thenReturn(webTarget);
         when(mockAlmaApiClient.getAlmaStage()).thenReturn(AlmaStage.SANDBOX2);
         when(mockAlmaApiClient.getContext()).thenReturn("");
         when(mockAlmaApiClient.getContextValue()).thenReturn("");
         when(webTarget.path(any())).thenReturn(webTarget);
-        AlmaBibsService almaVendorsServiceImplementation =
+        when(webTarget.path(any())).thenReturn(webTarget);
+        when(webTarget.request()).thenReturn(builder);
+        when(builder.accept(anyString())).thenReturn(builder);
+        when(builder.buildGet()).thenReturn(invocation);
+        Bib tempBib = new Bib();
+        tempBib.setTitle("test");
+        when(invocation.invoke((Class<Object>) any())).thenReturn(tempBib);
+        AlmaBibsServiceImplementation almaBibsServiceImplementation =
                 new AlmaBibsServiceImplementation(mockAlmaApiClient);
 
-        Bib bib = almaVendorsServiceImplementation.getBib(TEST_MMS_ID, "");
+        Bib bib = almaBibsServiceImplementation.getBib(TEST_MMS_ID, "");
         assertEquals("test", bib.getTitle());
     }
 
