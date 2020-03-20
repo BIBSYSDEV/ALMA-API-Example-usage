8df4ba63528bab752b2ca76664c5714632bb549f
Eirik Nilsen <eirik.nilsen@unit.no>
2020-03-20 12:35:50 +0100

    ALMA-40 Tester for holdings

---
+++ src/test/java/no/unit/alma/bibs/AlmaHoldingsServiceTest.java
@@ -0,0 +1,180 @@
+package no.unit.alma.bibs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import no.bibsys.alma.rest.holding.Holding;
+import no.bibsys.alma.rest.holdings.Holdings;
+import no.bibsys.alma.rest.items.Items;
+import no.unit.alma.commons.AlmaClient;
+import no.unit.alma.commons.AlmaStage;
+
+@ExtendWith(MockitoExtension.class)
+class AlmaHoldingsServiceTest {
+
+    private static final String CONTEXT = "exampleContext";
+    private static final String CONTEXT_VALUE = "exampleContextValue";
+    private static final AlmaStage STAGE = AlmaStage.SANDBOX2;
+
+    private static final Integer TOTAL_RECORD_COUNT = 1;
+    private static final String TEST_MMS_ID = "123456";
+    private static final String TEST_HOLDING_ID = "345678";
+    private static final String TEST_ORIGINATING_SYSTEM = "originating system";
+
+    @Mock
+    private AlmaClient mockAlmaApiClient;
+
+    @Mock
+    private WebTarget webTarget;
+
+    @Mock
+    private WebTarget bibsWebTarget;
+
+    @Mock
+    private WebTarget holdingsWebTarget;
+
+    @Mock
+    private Builder holdingsBuilder;
+
+    @Mock
+    private Builder itemsBuilder;
+
+    @Mock
+    private Invocation holdingsInvocation;
+
+    @Mock
+    private Invocation itemsInvocation;
+
+    @Test
+    void testGetHoldings() {
+        mockAlmaApi();
+
+        when(webTarget.path("bibs")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path(any())).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path("holdings")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.request()).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.accept(anyString())).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.buildGet()).thenReturn(holdingsInvocation);
+        Holdings tempHoldings = new Holdings();
+        tempHoldings.setTotalRecordCount(TOTAL_RECORD_COUNT);
+        when(holdingsInvocation.invoke((Class<Object>) any())).thenReturn(tempHoldings);
+        AlmaHoldingsService almaHoldingsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+
+        Holdings resultHoldings = almaHoldingsService.getHoldings(TEST_MMS_ID);
+        assertEquals(TOTAL_RECORD_COUNT, resultHoldings.getTotalRecordCount());
+
+    }
+
+    @Test
+    void testGetHolding() {
+        mockAlmaApi();
+
+        when(webTarget.path("bibs")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path(any())).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path("holdings")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.request()).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.accept(anyString())).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.buildGet()).thenReturn(holdingsInvocation);
+        Holding tempHolding = new Holding();
+        tempHolding.setOriginatingSystem(TEST_ORIGINATING_SYSTEM);
+        when(holdingsInvocation.invoke((Class<Object>) any())).thenReturn(tempHolding);
+        AlmaHoldingsService almaHoldingsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+
+        Holding resultHolding = almaHoldingsService.getHolding(TEST_MMS_ID, TEST_HOLDING_ID);
+        assertEquals(TEST_ORIGINATING_SYSTEM, resultHolding.getOriginatingSystem());
+    }
+
+    @Test
+    void testUpdateHolding() {
+        mockAlmaApi();
+
+        when(webTarget.path("bibs")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path(any())).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path("holdings")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.request()).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.accept(anyString())).thenReturn(holdingsBuilder);
+
+        Holding tempHolding = new Holding();
+        tempHolding.setOriginatingSystem(TEST_ORIGINATING_SYSTEM);
+        when(holdingsBuilder.buildPut(Entity.xml(tempHolding))).thenReturn(holdingsInvocation);
+        when(holdingsInvocation.invoke((Class<Object>) any())).thenReturn(tempHolding);
+        AlmaHoldingsService almaHoldingsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+
+        Holding resultHolding = almaHoldingsService.updateHolding(TEST_MMS_ID, tempHolding);
+        assertEquals(TEST_ORIGINATING_SYSTEM, resultHolding.getOriginatingSystem());
+    }
+
+    @Test
+    void testGetItems() {
+        mockAlmaApi();
+
+        when(webTarget.path("bibs")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path(any())).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.path("holdings")).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.queryParam(anyString(), any())).thenReturn(bibsWebTarget);
+        when(bibsWebTarget.request()).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.accept(anyString())).thenReturn(holdingsBuilder);
+        when(holdingsBuilder.buildGet()).thenReturn(holdingsInvocation);
+
+        Items tempItems = new Items();
+        tempItems.setTotalRecordCount(TOTAL_RECORD_COUNT);
+
+        when(holdingsInvocation.invoke((Class<Object>) any())).thenReturn(tempItems);
+
+        AlmaHoldingsService almaHoldingsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+
+        Items resultItems = almaHoldingsService.getItems(TEST_MMS_ID, TEST_HOLDING_ID, 10, 0);
+        assertEquals(TOTAL_RECORD_COUNT, resultItems.getTotalRecordCount());
+    }
+
+    @Test
+    void testGetAlmaStage() {
+        mockAlmaApi();
+        AlmaHoldingsService almaBibsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+        assertEquals(STAGE.getVaultAlmaStageName(), almaBibsService
+                .getAlmaStage().getVaultAlmaStageName());
+    }
+
+    @Test
+    void testGetContext() {
+        mockAlmaApi();
+        AlmaHoldingsService almaBibsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+        assertEquals(CONTEXT, almaBibsService.getContext());
+    }
+
+    @Test
+    void testGetContextValue() {
+        mockAlmaApi();
+        AlmaHoldingsService almaBibsService =
+                new AlmaHoldingsServiceImplementation(mockAlmaApiClient);
+        assertEquals(CONTEXT_VALUE, almaBibsService.getContextValue());
+    }
+
+    private void mockAlmaApi() {
+
+        when(mockAlmaApiClient.getWebTarget())
+                .thenReturn(webTarget);
+        when(mockAlmaApiClient.getAlmaStage()).thenReturn(STAGE);
+        when(mockAlmaApiClient.getContext()).thenReturn(CONTEXT);
+        when(mockAlmaApiClient.getContextValue()).thenReturn(CONTEXT_VALUE);
+    }
+
+}
