b26b7e9f6eae18a839d39d9177d98c08feee524f
Gregor Gabriel <gregor.gabriel@unit.no>
2020-03-24 13:00:12 +0100

    ALMA-24: autogenerate klasse without XMLRootElement. Da må man marshalle og unmarshalle litt annerledes

--- src/test/java/no/unit/alma/ApiHelper.java
+++ src/test/java/no/unit/alma/ApiHelper.java
@@ -3,16 +3,17 @@ package no.unit.alma;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.JAXBIntrospector;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.stream.StreamSource;
 
 import org.junit.jupiter.api.Test;
 
@@ -27,10 +28,10 @@ public class ApiHelper {
         String bibsFileName = "src/test/resources/bibs.xml";
         try {
             Bib bib = readXmlFromFile(fileName, Bib.class);
-            System.out.println(writeObject(bib));
+            System.out.println(writeObject(bib, Bib.class));
             Bibs bibs = readXmlFromFile(bibsFileName, Bibs.class);
-            System.out.println(writeObject(bibs));
-        } catch (IOException e) {
+            System.out.println(writeObject(bibs, Bibs.class));
+        } catch (XMLStreamException | FileNotFoundException e) {
             e.printStackTrace();
             fail(String.format("Error reading file %s", fileName));
         } catch (JAXBException e) {
@@ -39,27 +40,33 @@ public class ApiHelper {
         }
     }
 
-    public static <T> String writeObject(T object) throws JAXBException {
+    public static <T> String writeObject(T object, Class<T> type) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
 
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
         StringWriter sw = new StringWriter();
-        jaxbMarshaller.marshal(object, sw);
+        QName qName = new QName(object.getClass().getPackageName(), object.getClass().getName());
+        JAXBElement<T> root = new JAXBElement<T>(qName, type, object);
+
+        jaxbMarshaller.marshal(root, sw);
+
         return sw.toString();
     }
 
-    public static <T> T readXmlFromFile(String fileName, Class<T> type) throws IOException, JAXBException {
+    public static <T> T readXmlFromFile(String fileName, Class<T> type) throws JAXBException, FileNotFoundException, XMLStreamException {
 
         File file = new File(fileName);
         JAXBContext jaxbContext = JAXBContext.newInstance(type);
-        JAXBIntrospector jaxbIntrospector = jaxbContext.createJAXBIntrospector();
+//        JAXBIntrospector jaxbIntrospector = jaxbContext.createJAXBIntrospector();
 
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
-        T unmarshalledObject = (T) jaxbIntrospector.getValue(jaxbUnmarshaller.unmarshal(file));
+//        T unmarshalledObject = (T) jaxbIntrospector.getValue(jaxbUnmarshaller.unmarshal(parser, type));
 
-        return unmarshalledObject;
+        JAXBElement<T> root = jaxbUnmarshaller.unmarshal(new StreamSource(file), type);
+
+        return root.getValue();
     }
 }
