74ef8f33707e80f47af5e52ce6229271104dcdb0
Eirik Nilsen <eirik.nilsen@unit.no>
2020-03-25 08:55:25 +0100

    ALMA-49 Fjernet PND.GodClass fra SuppressWarnings

--- src/main/java/no/unit/alma/bibs/AlmaBibsService.java
+++ src/main/java/no/unit/alma/bibs/AlmaBibsService.java
@@ -177,7 +177,7 @@ public class AlmaBibsService {
         return createRemotePresentation(mmsId, input);
     }
 
-    @SuppressWarnings({ "PMD.GodClass", "PMD.ExcessiveParameterList" })
+    @SuppressWarnings({ "PMD.ExcessiveParameterList" })
     public Representation createRemoteRepresentation(long mmsId, String access, String remoteRepositoryId,
             String libraryCode, String url, String year, String month, String day, String volume, String issue,
             String number) {
@@ -209,7 +209,7 @@ public class AlmaBibsService {
         return createRemotePresentation(mmsIdAsString, input);
     }
 
-    @SuppressWarnings({ "PMD.GodClass", "PMD.ExcessiveParameterList" })
+    @SuppressWarnings({ "PMD.ExcessiveParameterList" })
     public Representation createRemoteRepresentation(String barcode, String access, String remoteRepositoryId,
             String libraryCode, String url, String year, String month, String day, String volume, String issue,
             String number) {
@@ -242,7 +242,7 @@ public class AlmaBibsService {
         return createRemotePresentation(mmsId, input);
     }
 
-    @SuppressWarnings({ "PMD.GodClass", "PMD.ExcessiveParameterList" })
+    @SuppressWarnings({ "PMD.ExcessiveParameterList" })
     public Representation createRemoteRepresentation(String barcode, String label, String access,
             String remoteRepositoryId, String libraryCode, String url, String year, String month, String day,
             String volume, String issue, String number) {
