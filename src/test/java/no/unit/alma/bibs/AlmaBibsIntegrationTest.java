7a5e51256677104dd7ef59439924b6241c8d94f1
Eirik Nilsen <eirik.nilsen@unit.no>
2020-03-26 07:27:23 +0100

    ALMA-38 Omskrevet etter commons-refaktorering

--- src/test/java/no/unit/alma/bibs/AlmaBibsIntegrationTest.java
+++ src/test/java/no/unit/alma/bibs/AlmaBibsIntegrationTest.java
@@ -3,19 +3,21 @@ package no.unit.alma.bibs;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.jupiter.api.Test;
 
-import no.unit.alma.commons.AlmaServiceFactory;
-import no.unit.alma.commons.AlmaStage;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import no.unit.alma.commons.AlmaClient;
 
 public class AlmaBibsIntegrationTest {
 
-    private static final AlmaServiceFactory almaServiceFactory =
-            new AlmaServiceFactory(JerseyClientBuilder.newClient());
     private static final String bibIdentifier = "99939650000541";
 
     @Test
     void testGetBib() {
         System.setProperty("java.net.preferIPv4Stack", "true");
-        AlmaBibsService almaBibsService = almaServiceFactory.getAlmaBibs("zz", AlmaStage.SANDBOX2);
+        Config config = ConfigFactory.defaultReference();
+        AlmaBibsService almaBibsService =
+                new AlmaBibsService(new AlmaClient(JerseyClientBuilder.newClient(), config, "zz"));
         System.out.println("alma bibs service setup...");
         almaBibsService.getBib(bibIdentifier, "");
     }
