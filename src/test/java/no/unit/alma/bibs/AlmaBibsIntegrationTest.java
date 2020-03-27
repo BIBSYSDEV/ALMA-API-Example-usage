33c2fc54239ed50ac7ca92232b30c87a1909d65e
Gregor Gabriel <gregor.gabriel@unit.no>
2020-03-27 15:39:41 +0100

    ALMA-51: feil fiks for unmarshalling WebServiceResult plus integrationTest

---
+++ src/test/java/no/unit/alma/bibs/AlmaBibsIntegrationTest.java
@@ -0,0 +1,54 @@
+package no.unit.alma.bibs;
+
+import no.unit.alma.commons.HttpStatusException;
+import no.unit.alma.generated.bibs.Bib;
+import no.unit.alma.generated.error.Error;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.jupiter.api.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import no.unit.alma.commons.AlmaClient;
+
+import javax.ws.rs.client.ResponseProcessingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AlmaBibsIntegrationTest {
+
+    private static final String bibIdentifier = "991334666044702202";
+    private static final String bad_bibIdentifier = "999999999999999999";
+    public static final String NOT_FOUND_ALMA_ERROR_CODE = "402203";
+    public static final String NOT_FOUND_ALMA_ERROR_MESSAGE = "Input parameters mmsId %s is not valid";
+
+    @Test
+    void testGetBib_not_found() {
+        Config config = ConfigFactory.defaultReference();
+        AlmaBibsService almaBibsService =
+                new AlmaBibsService(new AlmaClient(JerseyClientBuilder.newClient(), config, "guest"));
+        System.out.println("alma bibs service setup...");
+        try {
+            Bib bib = almaBibsService.getBib(bad_bibIdentifier, "");
+        } catch (ResponseProcessingException e) {
+            Error error = new Error();
+            if (e.getCause() instanceof HttpStatusException) {
+                HttpStatusException httpStatusException = (HttpStatusException) e.getCause();
+                if (httpStatusException.isWebserviceResult()) {
+                    try {
+                        error = httpStatusException.getWebServiceResult().getErrorList().getError().get(0);
+                    } catch (NullPointerException ex) {
+                        System.out.println(String.format("malformed response from Alma %s",
+                                httpStatusException.getResponseBody()));
+                    }
+                }
+            }
+            System.out.println(error.getErrorCode() + " : " + error.getErrorMessage());
+            assertEquals(NOT_FOUND_ALMA_ERROR_CODE, error.getErrorCode());
+            assertEquals(String.format(NOT_FOUND_ALMA_ERROR_MESSAGE, bad_bibIdentifier), error.getErrorMessage());
+        }
+        fail();
+    }
+
+}
