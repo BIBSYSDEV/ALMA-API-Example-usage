b197526616c45814a8331ca3439d54bf4d94e29c
Gregor Gabriel <gregor.gabriel@unit.no>
2020-03-29 17:28:47 +0200

    ALMA-51: maybe a better/smoother way to cope with Alma error response. ?

--- src/main/java/no/unit/alma/commons/HttpStatusException.java
+++ src/main/java/no/unit/alma/commons/HttpStatusException.java
@@ -1,16 +1,22 @@
 package no.unit.alma.commons;
 
+import no.unit.alma.generated.error.Error;
 import no.unit.alma.generated.error.WebServiceResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class HttpStatusException extends IOException {
 
+    private static final transient Logger log = LoggerFactory.getLogger(HttpStatusException.class);
     private transient int status;
     private transient String statusText;
     private transient String method;
     private transient String url;
     private transient String responseBody;
     private transient WebServiceResult webServiceResult;
+    private transient Error error;
 
     public HttpStatusException(int status, String statusText, String method, String url, String responseBody) {
         this(status, statusText, method, url, responseBody, null);
@@ -25,6 +31,7 @@ public class HttpStatusException extends IOException {
         this.url = url;
         this.responseBody = responseBody;
         this.webServiceResult = webServiceResult;
+        this.setError();
     }
 
     public int getStatus() {
@@ -53,5 +60,31 @@ public class HttpStatusException extends IOException {
 
     public WebServiceResult getWebServiceResult() {
         return webServiceResult;
+    }
+
+    private void setError() {
+        if (this.isWebserviceResult() && this.webServiceResult.isErrorsExist()) {
+            try {
+                this.error = this.getWebServiceResult().getErrorList().getError().get(0);
+            } catch (NullPointerException ex) {
+                log.error(String.format("Malformed response from Alma: %s.", this.getResponseBody()));
+            }
+        }
+    }
+
+    public boolean hasError() {
+        return this.error != null;
+    }
+
+    public String getAlmaErrorCode() {
+        return this.error.getErrorCode();
+    }
+
+    public String getAlmaErrorMessage() {
+        return this.error.getErrorMessage();
+    }
+
+    public String getAlmaErrorTrackingId() {
+        return this.error.getTrackingId();
     }
 }
--- src/test/java/no/unit/alma/bibs/AlmaBibsIntegrationTest.java
+++ src/test/java/no/unit/alma/bibs/AlmaBibsIntegrationTest.java
@@ -2,7 +2,6 @@ package no.unit.alma.bibs;
 
 import no.unit.alma.commons.HttpStatusException;
 import no.unit.alma.generated.bibs.Bib;
-import no.unit.alma.generated.error.Error;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +13,7 @@ import no.unit.alma.commons.AlmaClient;
 import javax.ws.rs.client.ResponseProcessingException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class AlmaBibsIntegrationTest {
@@ -21,7 +21,7 @@ public class AlmaBibsIntegrationTest {
     private static final String bibIdentifier = "991334666044702202";
     private static final String bad_bibIdentifier = "999999999999999999";
     public static final String NOT_FOUND_ALMA_ERROR_CODE = "402203";
-    public static final String NOT_FOUND_ALMA_ERROR_MESSAGE = "Input parameters mmsId %s is not valid";
+    public static final String NOT_FOUND_ALMA_ERROR_MESSAGE = "Input parameters mmsId %s is not valid.";
 
     @Test
     void testGetBib_not_found() {
@@ -29,26 +29,25 @@ public class AlmaBibsIntegrationTest {
         AlmaBibsService almaBibsService =
                 new AlmaBibsService(new AlmaClient(JerseyClientBuilder.newClient(), config, "guest"));
         System.out.println("alma bibs service setup...");
+        Bib bib = null;
         try {
-            Bib bib = almaBibsService.getBib(bad_bibIdentifier, "");
+            bib = almaBibsService.getBib(bad_bibIdentifier, "");
         } catch (ResponseProcessingException e) {
-            Error error = new Error();
             if (e.getCause() instanceof HttpStatusException) {
                 HttpStatusException httpStatusException = (HttpStatusException) e.getCause();
-                if (httpStatusException.isWebserviceResult()) {
-                    try {
-                        error = httpStatusException.getWebServiceResult().getErrorList().getError().get(0);
-                    } catch (NullPointerException ex) {
-                        System.out.println(String.format("malformed response from Alma %s",
-                                httpStatusException.getResponseBody()));
-                    }
+                if (httpStatusException.hasError()) {
+                    System.out.println(String.format("%s : %s -- %s", httpStatusException.getAlmaErrorCode(),
+                            httpStatusException.getAlmaErrorMessage(),
+                            httpStatusException.getAlmaErrorTrackingId()));
+                    assertEquals(NOT_FOUND_ALMA_ERROR_CODE, httpStatusException.getAlmaErrorCode());
+                    assertEquals(String.format(NOT_FOUND_ALMA_ERROR_MESSAGE, bad_bibIdentifier),
+                            httpStatusException.getAlmaErrorMessage());
                 }
+            } else {
+                fail();
             }
-            System.out.println(error.getErrorCode() + " : " + error.getErrorMessage());
-            assertEquals(NOT_FOUND_ALMA_ERROR_CODE, error.getErrorCode());
-            assertEquals(String.format(NOT_FOUND_ALMA_ERROR_MESSAGE, bad_bibIdentifier), error.getErrorMessage());
         }
-        fail();
+        assertNull(bib);
     }
 
 }
