5ce8bad937a0e529533f62035b56eb4ef0d56cec
Gregor Gabriel <gregor.gabriel@unit.no>
2020-04-03 08:13:09 +0200

    unused imports removed

--- src/test/java/no/unit/alma/commons/HttpStatusExceptionTest.java
+++ src/test/java/no/unit/alma/commons/HttpStatusExceptionTest.java
@@ -5,12 +5,7 @@ import no.unit.alma.generated.error.ErrorList;
 import no.unit.alma.generated.error.WebServiceResult;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
